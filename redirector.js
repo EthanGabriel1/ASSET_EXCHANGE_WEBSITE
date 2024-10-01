4f6e823c6f9eaad80a30e70882dc732c62a70d50
Itsan <etto.rolloque.swu@phinmaed.com>
2024-10-02 03:21:52 +0800

    Initial Commit

---
+++ redirector.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("storage-container").addEventListener("click", function() {
+        window.location.href = "AssetEx._Storage.html";
+    });
+});
